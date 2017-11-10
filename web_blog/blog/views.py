2946b5afec128a5d75df687aa1b416e43553a80e
Luxiaowuu <297251892@qq.com>
2017-11-10 19:31:35 +0800

    blog

---
+++ web_blog/blog/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Post,Category
+# no care about the error
+from comments.forms import CommentForm
+import markdown
+import pygments
+
+# Create your views here.
+
+def index(request):
+    post_list = Post.objects.all()
+    # print('---------------------------------------------------')
+    # print(post_list[1].get_absolute_url)
+    # print('-------------------------------------------------')
+    return render(request,'blog/index.html',context={
+        'post_list' : post_list
+    })
+
+def detail(request, pk):
+    post = get_object_or_404(Post,pk= pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                     'markdown.extensions.extra',
+                                     'markdown.extensions.codehilite',
+                                     'markdown.extensions.toc',
+                                  ])
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    count =len(comment_list)
+    return render(request,'blog/detail.html',context= {'post' : post,
+                                                       'form' : form,
+                                                       'comment_list' : comment_list,
+                                                       'comments_num' : count})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year= year,
+                                    created_time__month= month)
+    return render(request,'blog/index.html',context= {'post_list' : post_list})
+
+def category(request,pk):
+    cate = get_object_or_404(Category, pk= pk)
+    post_list = Post.objects.filter(category= cate)
+    return render(request,'blog/index.html',context= {'post_list' : post_list})
+
